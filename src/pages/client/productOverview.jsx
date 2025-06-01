40eadb192f363879a383d9139797907205cc6503
Janitha <janithalahiru23@gmail.com>
2025-06-01 07:49:27 +0530

    completed productOverview page 🚩

--- src/pages/client/productOverview.jsx
+++ src/pages/client/productOverview.jsx
@@ -2,6 +2,8 @@ import axios from "axios"
 import {useEffect, useState} from "react"
 import {useParams} from "react-router-dom"
 import toast from "react-hot-toast"
+import ImageSlider from "../../components/imageSlider"
+import Loading from "../../components/loading"
 
 export default function ProductOverviewPage(){
 
@@ -25,14 +27,49 @@ export default function ProductOverviewPage(){
     ,[]);
     return(
         <>
-        <div className="w-full h-full flex">
-           <div className="w-[50%] h-full">
+        {
+            status=="success"  && (   
+                <div className="w-full h-full flex">
+                    <div className="w-[50%] h-full flex justify-center items-center">
+                        <ImageSlider images={product.images}/>
+                    </div>
+                    <div className="w-[50%] flex justify-center items-center h-full">
+                        <div className="w-[500px] h-[600px] flex flex-col items-center">
+                            <h1 className="w-full text-center text-4xl text-secondary font-semibold">{product.name}
+                                {
+                                    product.altNames.map((altName,index)=>{
+                                        return(
+                                            <span key={index} className="text-4xl text-gray-600">{" | "+altName}</span>
+                                        )
+                                    })
+                                }
+                            </h1>
+                            <h1 className="w-full text-center my-2 text-md text-gray-600 font-semibold">{product.productId}</h1> 
+                            <p className="w-full text-center my-2 text-md text-gray-600 font-semibold">{product.description}</p> 
+                            {
+                                product.labelledPrice > product.price ?
+                                <div>
+                                    <span className="text-4xl mx-4 text-gray-500 line-through">{product.labelledPrice.toFixed(2)}</span>
+                                    <span className="text-4xl mx-4 font-bold text-accent">{product.price.toFixed(2)}</span>
+                                </div>
+                                :<span className="text-4xl mx-4 font-bold text-accent">{product.price.toFixed(2)}</span>
 
-           </div>
-           <div className="w-[50%] bg-blue-900 h-full">
-
-           </div>
-        </div>
+                            }
+                            <div className="w-full flex justify-center items-center mt-4">
+                                <button className="w-[200px] h-[50px] mx-4 cursor-pointer bg-accent text-white rounded-2xl hover:bg-accent/80 transition-all duration-300">Add to Cart</button>
+                                <button className="w-[200px] h-[50px] mx-4 cursor-pointer bg-accent text-white rounded-2xl hover:bg-accent/80 transition-all duration-300">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+        {
+            status=="loading" && <Loading/>
+        }
         </>
+        
+
+        
     )
 }
